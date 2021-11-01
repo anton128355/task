64bbd28c412af5d59d4670c833116ac99bb11c14
Anton <anton128355@gmail.com>
2021-11-01 21:36:25 +0200

    Fix some problem

--- technical_task.py
+++ technical_task.py
@@ -1,22 +1,25 @@
-def tote(bet, result):
-    # result_fact - ничья, победа или поражение первой команды.
-    result_fact, lst_points, lst_players  = (int(result[0]) - int(result[2])), [], [] 
-    
-    for player in bet:
-        # result_presumptive - преположительный результат матча
-        result_presumptive = int(bet[player][0]) - int(bet[player][2])
+def totalizator(bet, result):
 
-        if bet[player] == result:
+    # result_difference - разница между фактическими очками. Ничья (0), победа (+) или поражение (-) первой команды.
+    result_difference = (int(result.split(":")[0]) - int(result.split(":")[1])) 
+    lst_points, lst_players = [], []
+
+    for res in bet.values():
+
+        if res == result:
             lst_points.append(2)
+            continue
+
+        # result_presumptive - разница между преположительными очками. Ничья (0), победа (+) или поражение (-) первой команды.
+        result_presumptive = int(res.split(":")[0]) - int(res.split(":")[1])
         
-        # 1 - ничья, 2 - победа первой команды, 3 - поражение первой команды. 
-        elif (result_fact == result_presumptive == 0) or (result_fact > 0 and result_presumptive > 0) or (result_fact < 0 and result_presumptive < 0):
-             lst_points.append(1)
-             
+        # 1 - ничья, 2 - победа или поражение первой команды.
+        if (result_difference == result_presumptive == 0) or ((result_difference * result_presumptive) > 0):
+
+            lst_points.append(1)
+
         else:
             lst_points.append(0)
 
-    # Словарь с игроками, в качестве ключей и их очками, в качестве значений. 
-    print(dict(zip(bet.keys(), lst_points)))
-
-tote({"user1": "3:1", "user2": "3:1", "user3": "3:1"}, "3:1")
+    # Словарь с игроками, в качестве ключей, и их очками, в качестве значений. 
+    print((dict(zip(bet.keys(), lst_points))))
